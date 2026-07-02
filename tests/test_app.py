13ff4498d967a8328eb364e3b545d5e5d7143ebd
Septiamf <septiamfmf@gmail.com>
2026-07-02 14:18:59 +0700

    Add Dockerfile

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+# Menambahkan folder utama project ke Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app
+
+client = app.test_client()
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.get_json()["status"] == "healthy"
